78606bf9bfb94373425fccf1bfb02fe40d79308a
davron <mrz.davron@gmail.com>
2024-07-21 23:21:58 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from app import PyTezApp
+app = PyTezApp()
+
+# def app(environ, start_response):
+#     print(environ)
+#     status = "200 OK"
+#     headers = [("Content-type", "text/plain")]
+#     start_response(status, headers)
+#     return [b"Hello world"]
+
+
+@app.route('/home')
+def home(request, response):
+    response.text = "Hello from home page"
+
+
+@app.route('/about')
+def about(request, response):
+    response.text = "Hello from about page"
+
+
+@app.route('/hello/{name}')
+def greeting(request, response, name):
+    response.text = f"Hello from {name}"
+
+
+@app.route('/books')
+class Books:
+
+    def get(self, request, response):
+        response.text = "Books page"
+
+    def post(self, request, response):
+        response.text = "Endpoint to create a book"
+
+
+def new_handler(req, resp):
+    resp.text = 'from new handler'
+
+
+app.add_route('/new-handler', new_handler)
+
+
+@app.route('/template')
+def template_handler(req, resp):
+    resp.body = app.template(
+        "home.html",
+        context={"new_title": "Best title", "new_body": "best title"}
+    )
+
+
+def on_exception(req, resp, exe):
+    resp.text = 'something bad happened'
+
+
+app.add_exception_handler(on_exception)
+
+
+@app.route('/exception')
+def exception_throwing_handler(req, resp):
+    raise AttributeError('some exception')
---
+++ test_app.py
@@ -0,0 +1,118 @@
+import pytest
+
+
+def test_basic_route_adding(app):
+
+    @app.route('/home')
+    def home(req, res):
+        res.text = "hello from home"
+
+
+def test_duplicate_routes_throws_exception(app):
+    @app.route('/home')
+    def home(req, res):
+        res.text = "hello from home"
+
+    with pytest.raises(AssertionError):
+        @app.route('/home')
+        def home2(req, res):
+            res.text = "hello from home2"
+
+
+def test_requests_can_be_sent_by_test_client(app, test_client):
+    @app.route('/home')
+    def home(req, res):
+        res.text = "hello from home"
+
+    response = test_client.get('http://testserver/home')
+    assert response.text == "hello from home"
+
+
+def test_parameterized_routing(app, test_client):
+    @app.route('/hello/{name}')
+    def greeting(request, response, name):
+        response.text = f"Hello {name}"
+
+    assert test_client.get('http://testserver/hello/davron').text == 'Hello davron'
+    assert test_client.get('http://testserver/hello/matthew').text == 'Hello matthew'
+
+
+def test_default_response(test_client):
+    response = test_client.get('http://testserver/nonexistent')
+    assert response.text == 'Not Found'
+    assert response.status_code == 404
+
+
+def test_class_based_get(app, test_client):
+    @app.route('/books')
+    class Books:
+
+        def get(self, request, response):
+            response.text = "Books page"
+
+    assert test_client.get('http://testserver/books').text == 'Books page'
+
+
+def test_class_based_post(app, test_client):
+    @app.route('/books')
+    class Books:
+        def post(self, request, response):
+            response.text = "Endpoint to create a book"
+
+    assert test_client.post('http://testserver/books').text == 'Endpoint to create a book'
+
+
+def test_class_based_method_not_allowed(app, test_client):
+    @app.route('/books')
+    class Books:
+
+        def get(self, request, response):
+            response.text = "Books page"
+
+    response = test_client.post('http://testserver/books')
+    assert response.text == 'Method Not Allowed'
+    assert response.status_code == 405
+
+
+def test_alternative_route_adding(app, test_client):
+    def new_handler(req, resp):
+        resp.text = 'from new handler'
+
+    app.add_route('/new-handler', new_handler)
+    assert test_client.get('http://testserver/new-handler').text == 'from new handler'
+
+
+def test_template_handler(app, test_client):
+    @app.route('/test-template')
+    def template_handler(req, resp):
+        resp.body = app.template(
+            "test.html",
+            context={"new_title": "Best title", "new_body": "Best body"}
+        )
+    response = test_client.get('http://testserver/test-template')
+    assert "Best body" in response.text
+    assert "Best title" in response.text
+    assert "text/html" in response.headers["Content-Type"]
+
+
+def test_custom_exception_handler(app, test_client):
+    def on_exception(req, resp, exe):
+        resp.text = 'something bad happened'
+
+    app.add_exception_handler(on_exception)
+
+    @app.route('/exception')
+    def exception_throwing_handler(req, resp):
+        raise AttributeError('some exception')
+
+    response = test_client.get('http://testserver/exception')
+    assert response.text == 'something bad happened'
+
+
+def test_non_existent_static_file(test_client):
+    assert test_client.get('http://testserver/nonexistent.css').status_code == 404
+
+
+def test_serving_static_files(test_client):
+    response = test_client.get('http://testserver/test.css')
+    assert response.text == "body {background-color: chocolate;}"
